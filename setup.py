45be61471b9ded37fdc552fff80627946e22e88f
George Sakkis <gsakkis@users.noreply.github.com>
2009-12-22 02:00:30 +0200

    Updated setup.py, MANIFEST.in, LICENCE

--- setup.py
+++ setup.py
@@ -3,25 +3,25 @@
 from distutils.core import setup
 
 setup(
-    name            = 'pytrie',
+    name            = 'PyTrie',
     version         = '0.1',
-    py_modules      = ['pytrie'],
     author          = 'George Sakkis',
     author_email    = 'george.sakkis@gmail.com',
-    url             = 'http://code.google.com/p/pytrie/',
-    description     = 'Trie data structure',
+    url             = 'http://bitbucket.org/gsakkis/pytrie/',
+    description     = 'A pure Python implementation of the trie data structure.',
     long_description=
-'''*pytrie* exposes a single Python class, *triedict*, an implementation of
-tries that extends the mapping interface. A trie (from re**trie**val), is a
-multi-way tree structure useful for storing strings over an alphabet. **triedicts**
-generalize the standard tries in that the keys are not restricted to strings but
-can be any finite iterables of hashable objects, e.g. lists of numbers.
+'''A *trie* is an ordered tree data structure that is used to store a mapping
+where the keys are sequences, usually strings over an alphabet. In addition to
+implementing the mapping interface, tries allow finding the items for a given
+prefix, and vice versa, finding the items whose keys are prefixes of a given key.
 ''',
     classifiers     = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    py_modules      = ['pytrie'],
 )
