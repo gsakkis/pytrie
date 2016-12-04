6c5d86c6bd7c6490d34926f6065cbf998a88fc6c
George Sakkis <george.sakkis@gmail.com>
2016-12-04 21:04:50 +0200

    Lint pytrie

--- pytrie.py
+++ pytrie.py
@@ -1,4 +1,5 @@
-''':mod:`pytrie` is a pure Python implementation of the
+"""
+:mod:`pytrie` is a pure Python implementation of the
 `trie <http://en.wikipedia.org/wiki/Trie>`_ (prefix tree) data structure.
 
 A *trie* is a tree data structure that is used to store a mapping where the keys
@@ -9,13 +10,13 @@ common special case, finding the longest-prefix item is also supported.
 
 Algorithmically, tries are more efficient than binary search trees (BSTs) both
 in lookup time and memory when they contain many keys sharing relatively few
-prefixes. Unlike hash tables, trie keys don't need to be hashable. In the current
-implementation, a key can be any finite iterable with hashable elements.
+prefixes. Unlike hash tables, trie keys don't need to be hashable. In the
+current implementation, a key can be any finite iterable with hashable elements.
 
 Usage
 -----
->>> from pytrie import SortedStringTrie as trie
->>> t = trie(an=0, ant=1, all=2, allot=3, alloy=4, aloe=5, are=6, be=7)
+>>> from pytrie import SortedStringTrie as Trie
+>>> t = Trie(an=0, ant=1, all=2, allot=3, alloy=4, aloe=5, are=6, be=7)
 >>> t
 SortedStringTrie({'all': 2, 'allot': 3, 'alloy': 4, 'aloe': 5, 'an': 0, 'ant': 1, 'are': 6, 'be': 7})
 >>> t.keys(prefix='al')
@@ -38,7 +39,7 @@ KeyError
 ['all', 'allot']
 >>> list(t.iter_prefix_items('antonym'))
 [('an', 0), ('ant', 1)]
-'''
+"""
 
 __all__ = ['Trie', 'StringTrie', 'SortedTrie', 'SortedStringTrie', 'Node']
 
@@ -52,11 +53,13 @@ PY3 = sys.version_info[0] == 3
 if PY3:
     def itervalues(d):
         return d.values()
+
     def iteritems(d):
         return d.items()
 else:
     def itervalues(d):
         return d.itervalues()
+
     def iteritems(d):
         return d.iteritems()
 
@@ -67,15 +70,16 @@ class NULL(object):
 
 
 class Node(object):
-    '''Trie node class.
+    """Trie node class.
 
     Subclasses may extend it to replace :attr:`ChildrenFactory` with a different
-    mapping class (e.g. `sorteddict <http://pypi.python.org/pypi/sorteddict/>`_).
+    mapping class (e.g. `sorteddict <http://pypi.python.org/pypi/sorteddict/>`_)
 
-    :ivar value: The value of the key corresponding to this node or :const:`NULL`
-        if there is no such key.
+    :ivar value: The value of the key corresponding to this node or
+        :const:`NULL` if there is no such key.
     :ivar children: A ``{key-part : child-node}`` mapping.
-    '''
+    """
+
     __slots__ = ('value', 'children')
 
     #: A callable for creating a new :attr:`children` mapping.
@@ -86,7 +90,7 @@ class Node(object):
         self.children = self.ChildrenFactory()
 
     def numkeys(self):
-        '''Return the number of keys in the subtree rooted at this node.'''
+        """Return the number of keys in the subtree rooted at this node."""
         return (int(self.value is not NULL) +
                 sum(child.numkeys() for child in itervalues(self.children)))
 
@@ -103,18 +107,18 @@ class Node(object):
         return clone
 
     def __getstate__(self):
-        return (self.value, self.children)
+        return self.value, self.children
 
     def __setstate__(self, state):
         self.value, self.children = state
 
 
 class Trie(MutableMapping):
-    '''Base trie class.
+    """Base trie class.
 
     As with regular dicts, keys are not necessarily returned sorted. Use
     :class:`SortedTrie` if sorting is required.
-    '''
+    """
 
     #: Callable for forming a key from its parts.
     KeyFactory = tuple
@@ -123,20 +127,22 @@ class Trie(MutableMapping):
     NodeFactory = Node
 
     def __init__(self, *args, **kwargs):
-        '''Create a new trie.
+        """Create a new trie.
 
         Parameters are the same with ``dict()``.
-        '''
+        """
         self._root = self.NodeFactory()
 
         self.update(*args, **kwargs)
 
     @classmethod
     def fromkeys(cls, iterable, value=None):
-        '''Create a new trie with keys from ``iterable`` and values set to ``value``.
+        """
+        Create a new trie with keys from ``iterable`` and values set to
+        ``value``.
 
         Parameters are the same with ``dict.fromkeys()``.
-        '''
+        """
         d = cls()
         for key in iterable:
             d[key] = value
@@ -145,26 +151,27 @@ class Trie(MutableMapping):
     #----- trie-specific methods -----------------------------------------------
 
     def longest_prefix(self, key, default=NULL):
-        '''Return the longest key in this trie that is a prefix of ``key``.
+        """Return the longest key in this trie that is a prefix of ``key``.
 
         If the trie doesn't contain any prefix of ``key``:
           - if ``default`` is given, return it
           - otherwise raise ``KeyError``
-        '''
-        try: return self.longest_prefix_item(key)[0]
+        """
+        try:
+            return self.longest_prefix_item(key)[0]
         except KeyError:
             if default is not NULL:
                 return default
             raise
 
     def longest_prefix_value(self, key, default=NULL):
-        '''Return the value associated with the longest key in this trie that is
+        """Return the value associated with the longest key in this trie that is
         a prefix of ``key``.
 
         If the trie doesn't contain any prefix of ``key``:
           - if ``default`` is given, return it
           - otherwise raise ``KeyError``
-        '''
+        """
         current = self._root
         longest_prefix_value = NULL
         for part in key:
@@ -182,13 +189,13 @@ class Trie(MutableMapping):
             raise KeyError
 
     def longest_prefix_item(self, key, default=NULL):
-        '''Return the item (``(key,value)`` tuple) associated with the longest
+        """Return the item (``(key,value)`` tuple) associated with the longest
         key in this trie that is a prefix of ``key``.
 
         If the trie doesn't contain any prefix of ``key``:
           - if ``default`` is given, return it
           - otherwise raise ``KeyError``
-        '''
+        """
         prefix = []
         append = prefix.append
         current = self._root
@@ -205,14 +212,17 @@ class Trie(MutableMapping):
                 max_non_null_index = i
         if longest_prefix_value is not NULL:
             del prefix[max_non_null_index+1:]
-            return (self.KeyFactory(prefix), longest_prefix_value)
+            return self.KeyFactory(prefix), longest_prefix_value
         elif default is not NULL:
             return default
         else:
             raise KeyError
 
     def iter_prefixes(self, key):
-        'Return an iterator over the keys of this trie that are prefixes of ``key``.'
+        """
+        Return an iterator over the keys of this trie that are prefixes of
+        ``key``.
+        """
         key_factory = self.KeyFactory
         prefix = []
         append = prefix.append
@@ -226,9 +236,9 @@ class Trie(MutableMapping):
                 yield key_factory(prefix)
 
     def iter_prefix_values(self, key):
-        '''Return an iterator over the values of this trie that are associated
+        """Return an iterator over the values of this trie that are associated
         with keys that are prefixes of ``key``.
-        '''
+        """
         node = self._root
         for part in key:
             node = node.children.get(part)
@@ -238,9 +248,9 @@ class Trie(MutableMapping):
                 yield node.value
 
     def iter_prefix_items(self, key):
-        '''Return an iterator over the items (``(key,value)`` tuples) of this
+        """Return an iterator over the items (``(key,value)`` tuples) of this
         trie that are associated with keys that are prefixes of ``key``.
-        '''
+        """
         key_factory = self.KeyFactory
         prefix = []
         append = prefix.append
@@ -256,81 +266,84 @@ class Trie(MutableMapping):
     #----- extended mapping API methods ----------------------------------------
 
     def keys(self, prefix=None):
-        '''Return a list of this trie's keys.
+        """Return a list of this trie's keys.
 
         :param prefix: If not None, return only the keys prefixed by ``prefix``.
-        '''
+        """
         return list(self.iterkeys(prefix))
 
     def values(self, prefix=None):
-        '''Return a list of this trie's values.
+        """Return a list of this trie's values.
 
         :param prefix: If not None, return only the values associated with keys
             prefixed by ``prefix``.
-        '''
+        """
         return list(self.itervalues(prefix))
 
     def items(self, prefix=None):
-        '''Return a list of this trie's items (``(key,value)`` tuples).
+        """Return a list of this trie's items (``(key,value)`` tuples).
 
         :param prefix: If not None, return only the items associated with keys
             prefixed by ``prefix``.
-        '''
+        """
         return list(self.iteritems(prefix))
 
     def iterkeys(self, prefix=None):
-        '''Return an iterator over this trie's keys.
+        """Return an iterator over this trie's keys.
 
         :param prefix: If not None, yield only the keys prefixed by ``prefix``.
-        '''
-        return (key for key,value in self.iteritems(prefix))
+        """
+        return (key for key, value in self.iteritems(prefix))
 
     def itervalues(self, prefix=None):
-        '''Return an iterator over this trie's values.
+        """Return an iterator over this trie's values.
 
         :param prefix: If not None, yield only the values associated with keys
             prefixed by ``prefix``.
-        '''
-        def generator(node, NULL=NULL):
-            if node.value is not NULL:
+        """
+        def generator(node, null=NULL):
+            if node.value is not null:
                 yield node.value
             for part, child in iteritems(node.children):
                 for subresult in generator(child):
                     yield subresult
         if prefix is None:
-            node = self._root
+            root = self._root
         else:
-            node = self._find(prefix)
-            if node is None:
-                node = self.NodeFactory()
-        return generator(node)
+            root = self._find(prefix)
+            if root is None:
+                root = self.NodeFactory()
+        return generator(root)
 
     def iteritems(self, prefix=None):
-        '''Return an iterator over this trie's items (``(key,value)`` tuples).
+        """Return an iterator over this trie's items (``(key,value)`` tuples).
 
         :param prefix: If not None, yield only the items associated with keys
             prefixed by ``prefix``.
-        '''
+        """
         parts = []
         append = parts.append
+
         def generator(node, key_factory=self.KeyFactory, parts=parts,
-                      append=append, NULL=NULL):
-            if node.value is not NULL:
+                      append=append, null=NULL):
+            if node.value is not null:
                 yield (key_factory(parts), node.value)
             for part, child in iteritems(node.children):
                 append(part)
                 for subresult in generator(child):
                     yield subresult
                 del parts[-1]
-        node = self._root
+
+        root = self._root
         if prefix is not None:
             for part in prefix:
                 append(part)
-                node = node.children.get(part)
-                if node is None:
-                    node = self.NodeFactory()
+                root = root.children.get(part)
+                if root is None:
+                    root = self.NodeFactory()
                     break
-        return generator(node)
+
+        return generator(root)
 
     #----- original mapping API methods ----------------------------------------
 
@@ -355,13 +368,13 @@ class Trie(MutableMapping):
 
     def __setitem__(self, key, value):
         node = self._root
-        Node = self.NodeFactory
+        factory = self.NodeFactory
         for part in key:
-            next = node.children.get(part)
-            if next is None:
-                node = node.children.setdefault(part, Node())
+            next_node = node.children.get(part)
+            if next_node is None:
+                node = node.children.setdefault(part, factory())
             else:
-                node = next
+                node = next_node
         node.value = value
 
     def __delitem__(self, key):
@@ -369,7 +382,7 @@ class Trie(MutableMapping):
         append = nodes_parts.append
         node = self._root
         for part in key:
-            append((node,part))
+            append((node, part))
             node = node.children.get(part)
             if node is None:
                 break
@@ -378,14 +391,14 @@ class Trie(MutableMapping):
         node.value = NULL
         pop = nodes_parts.pop
         while node.value is NULL and not node.children and nodes_parts:
-            node,part = pop()
+            node, part = pop()
             del node.children[part]
 
     def clear(self):
         self._root.children.clear()
 
     def copy(self):
-        clone = copy(super(Trie,self))
+        clone = copy(super(Trie, self))
         clone._root = copy(self._root)
         return clone
 
@@ -407,7 +420,7 @@ class Trie(MutableMapping):
 
 
 class StringTrie(Trie):
-    '''A more appropriate for string keys :class:`Trie`.'''
+    """A more appropriate for string keys :class:`Trie`."""
     KeyFactory = ''.join
 
 
@@ -428,21 +441,24 @@ class _SortedNode(Node):
 
 
 class SortedTrie(Trie):
-    '''A :class:`Trie` that returns its keys (and associated values/items) sorted.
+    """
+    A :class:`Trie` that returns its keys (and associated values/items) sorted.
 
     .. note::
-        This implementation does not keep the keys sorted internally; instead it
-        sorts them every time a method returning a list or iterator (e.g.
-        :meth:`keys`) is called. In cases where a trie is relatively stable
-        (few inserts/deletes) and is iterated often, it is probably more efficient
-        to use a :attr:`NodeFactory` based on a sorted dict such as
-        `sorteddict <http://pypi.python.org/pypi/sorteddict/>`_.
-    '''
+    This implementation does not keep the keys sorted internally; instead it
+    sorts them every time a method returning a list or iterator (e.g.
+    :meth:`keys`) is called. In cases where a trie is relatively stable
+    (few inserts/deletes) and is iterated often, it is probably more efficient
+    to use a :attr:`NodeFactory` based on a sorted dict such as
+    `sorteddict <http://pypi.python.org/pypi/sorteddict/>`_.
+    """
     NodeFactory = _SortedNode
 
 
 class SortedStringTrie(SortedTrie, StringTrie):
-    'A :class:`Trie` that is both a :class:`StringTrie` and a :class:`SortedTrie`.'
+    """
+    A :class:`Trie` that is both a :class:`StringTrie` and a :class:`SortedTrie`
+    """
 
 
 if __name__ == '__main__':
