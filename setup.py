de0940c823a9fdbb717a770ddf6a30051ba9b246
George Sakkis <george.sakkis@gmail.com>
2016-12-04 16:45:42 +0200

    Specify test suite in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name            = 'PyTrie',
-    version         = '0.2',
-    author          = 'George Sakkis',
-    author_email    = 'george.sakkis@gmail.com',
-    url             = 'http://bitbucket.org/gsakkis/pytrie/',
-    description     = 'A pure Python implementation of the trie data structure.',
-    long_description=
-'''A *trie* is an ordered tree data structure that is used to store a mapping
-where the keys are sequences, usually strings over an alphabet. In addition to
-implementing the mapping interface, tries allow finding the items for a given
-prefix, and vice versa, finding the items whose keys are prefixes of a given key.
-''',
-    classifiers     = [
+    name='PyTrie',
+    version='0.2',
+    author='George Sakkis',
+    author_email='george.sakkis@gmail.com',
+    url='http://bitbucket.org/gsakkis/pytrie/',
+    description='A pure Python implementation of the trie data structure.',
+    long_description='''A *trie* is an ordered tree data structure that is used
+    to store a mapping where the keys are sequences, usually strings over an
+    alphabet. In addition to implementing the mapping interface, tries allow
+    finding the items for a given prefix, and vice versa, finding the items
+    whose keys are prefixes of a given key.''',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -24,5 +23,6 @@ prefix, and vice versa, finding the items whose keys are prefixes of a given key
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    py_modules      = ['pytrie'],
+    py_modules=['pytrie'],
+    test_suite='tests',
 )
