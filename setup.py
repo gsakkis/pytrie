b736fd5e8274011b1bc61f82c5e1a5e1dd4338b2
Shahar Evron <shahar.evron@gmail.com>
2020-10-21 21:37:50 +0300

    Fix how the project description is viewed in pypi
    This explicitly sets the content type of the description to markdown, so that it is properly rendered in pypi.org

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     url='https://github.com/gsakkis/pytrie/',
     description='A pure Python implementation of the trie data structure.',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
